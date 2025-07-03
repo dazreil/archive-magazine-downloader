fea43c7889e4ebc31316bed2ab62fdb739ff8492
dazreil <dazreil@gmail.com>
2025-07-03 20:51:23 +0100

    Initial commit

--- api/search.js
+++ api/search.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 export default async function handler(req, res) {
-  const searchTerm = req.query.q || "";
-  const url = `https://archive.org/advancedsearch.php?q=collection%3A%22magazinerack%22+AND+title%3A${encodeURIComponent(searchTerm)}&fl[]=identifier&fl[]=title&rows=10&output=json`;
+  const searchTerm = req.query.q || "*";
+  const query = `collection:"magazinerack" ${searchTerm}`;
+  const url = `https://archive.org/advancedsearch.php?q=${encodeURIComponent(query)}&fl[]=identifier&fl[]=title&rows=20&output=json`;
 
   const { data } = await axios.get(url);
   const docs = data.response.docs;
